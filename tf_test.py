7233e59e0a78a65982b55cd91c428af39f4c4d7d
huajh7 <huajh7@gmail.com>
2017-03-30 20:26:30 +0800

    fenzhi1

--- tf_test.py
+++ tf_test.py
@@ -8,8 +8,10 @@ a = tf.constant(10)
 b = tf.constant(32)
 print(sess.run(a + b))
 print('hello world.')
+print('fenzhi1')
 a = np.array([10, 20])
 
 # D:/ProgramData/Anaconda3/envs/python35/Scripts/ipython
 # machine learnig
 # hello git 
+# fenzhi1
